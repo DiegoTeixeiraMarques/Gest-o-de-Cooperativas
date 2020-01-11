7e64da7db317a0dcd6c3f3b680f12958e04b6884
DiegoTeixeiraMarques <diegodtm@live.com>
2020-01-11 20:29:47 -0300

    aa

--- CoopSys/producao/views.py
+++ CoopSys/producao/views.py
@@ -46,49 +46,44 @@ def index(request):
 
 def pegarPeso():
 
-    __message = str(chr(5))
-    _serial = serial.Serial(
-        'com1',
-        baudrate=9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1,
-        writeTimeout=2
-    )
+    try:
+        __message = str(chr(5))
+        _serial = serial.Serial(
+            'com1',
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1,
+            writeTimeout=2
+        )
 
-    print(_serial)
+        print(_serial)
 
-    # if True:
-    if _serial.isOpen():
-        print("aberto")
-        _serial.write(__message.encode('ascii'))
-        _serial.flushInput()
-        weight = _serial.read(14)
-        str_weight = str(weight.decode())
-        # new_weight = ["0","3",".","1","2"]
-        new_weight = []
-        for x in range(len(str_weight)):
-            if 0 < x < 7:
-                new_weight.append(str_weight[x])
+        # if True:
+    
+        if _serial.isOpen():
+            print("aberto")
+            _serial.write(__message.encode('ascii'))
+            _serial.flushInput()
+            weight = _serial.read(14)
+            str_weight = str(weight.decode())
+            # new_weight = ["0","3",".","1","2"]
+            new_weight = []
+            for x in range(len(str_weight)):
+                if 0 < x < 7:
+                    new_weight.append(str_weight[x])
 
-        _serial.close()
+            _serial.close()
 
-        new_weight = "".join(new_weight)
-    else:
-        print("fechado")
+            new_weight = "".join(new_weight)
+        else:
+            print("fechado")
+            new_weight = 20
+    except:
+        print("exceção")
         new_weight = 20
     
 
     return (new_weight)
 
-def ponto(request):
-
-    template_name = 'index.html'
-    context = {
-        'producao': producao,
-        'msg': msg,
-        'data_atual': data_atual
-    }
-
-    return render(request, template_name, context)
