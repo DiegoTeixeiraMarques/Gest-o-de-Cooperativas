90f4a25b2b97f7d41e03533e92d11e6775beeb90
DiegoTeixeiraMarques <diegodtm@live.com>
2020-02-14 01:09:17 -0300

    Tela de relatorios, acrescimentode colunas nos relatorio]

--- CoopSys/producao/views.py
+++ CoopSys/producao/views.py
@@ -63,7 +63,9 @@ def index(request):
 def relatorio(request):
 
     template_name = 'relatorios.html'
+    data_atual = date.today()
     context = {
+        'data_atual': data_atual
     }
 
     return render(request, template_name, context)
@@ -150,74 +152,86 @@ def exportar_producao2(request):
 def exportar_producao(request):
 
     response = HttpResponse(content_type='application/ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="Producao_do_mes.xls"'
+    response['Content-Disposition'] = 'attachment; filename="Relatorio de Producao.xls"'
     wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet('Fechamento_mes')
+    ws = wb.add_sheet('Produtividade')
 
-    #try:
+    try:
 
-    # Sheet header, first row
-    font_style = xlwt.XFStyle()
-    font_style.font.bold = True
+        # Sheet header, first row
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
 
-    row_num = 0
-    columns = []
-    datas = []
-    dataInicial = date.fromordinal(date.today().toordinal()-2)
-    dataFinal = date.fromordinal(dataInicial.toordinal()+7)
-    qtdDias = (dataFinal - dataInicial).days
-    data = Calendario.objects.get(data=dataInicial)
+        dataInicialTeste = request.POST.get('data1')
+        print(dataInicialTeste)
+        print(type(dataInicialTeste))
 
-    # Acrescentando as duas primeiras colunas
-    columns.append('Código')
-    columns.append('Funcionário')
-    columns.append('Supervisor')
-    columns.append('Empresa')
-    columns.append('Dias Considerados')
-    columns.append('Dias Úteis')
-    columns.append('Média')
-    columns.append('Efetiva')
+        row_num = 0
+        columns = []
+        datas = []
+        dataInicial = date.fromordinal(date.today().toordinal()-2)
+        dataFinal = date.fromordinal(dataInicial.toordinal()+7)
+        qtdDias = (dataFinal - dataInicial).days
+        data = Calendario.objects.get(data=dataInicial)
+        vrPago = 0.90
+
+        # Acrescentando as duas primeiras colunas
+        columns.append('Código')
+        columns.append('Funcionário')
+        columns.append('Supervisor')
+        columns.append('Empresa')
+        columns.append('Meta')
+        columns.append('Produção Total')
+        columns.append('Vr Considerado')
+        columns.append('Dias Considerados')
+        columns.append('Dias Úteis')
+        columns.append('Média')
+        columns.append('Efetiva')
+        
+
+        # Colocando datas numa lista
+        for i in range(qtdDias):
+            datas.append(date.fromordinal(data.data.toordinal()+i))
+            if datas[i].weekday() == 5:
+                columns.append('Sábado')
+            elif datas[i].weekday() == 6:
+                columns.append('Domingo')
+            else:
+                columns.append(format(datas[i], "%d/%m/%Y"))
+
+        # Colocando as datas como colunas da planilha
+        for col_num in range(len(columns)):
+            ws.write(row_num, col_num, columns[col_num], font_style)
+
+        # Sheet body, remaining rows
+        font_style = xlwt.XFStyle() 
+
+        funcionarios = buscarFuncionarios()
+        funcionarios = buscarSupervisores_Empresa(funcionarios)
+        funcionarios = buscarProducoes(funcionarios, datas, vrPago)
+
+        for row in funcionarios:
+            row_num += 1
+            for col_num in range(len(row)):
+                ws.write(row_num, col_num, row[col_num], font_style)          
+        
+        wb.save(response)
+        return response
     
-
-    # Colocando datas numa lista
-    for i in range(qtdDias):
-        datas.append(date.fromordinal(data.data.toordinal()+i))
-        columns.append(format(datas[i], "%d/%m/%Y"))
-
-    # Colocando as datas como colunas da planilha
-    for col_num in range(len(columns)):
-        ws.write(row_num, col_num, columns[col_num], font_style)
-
-    # Sheet body, remaining rows
-    font_style = xlwt.XFStyle() 
-
-    funcionarios = buscarFuncionarios()
-
-    funcionarios = buscarSupervisores_Empresa(funcionarios)
-    funcionarios = buscarProducoes(funcionarios, datas)
-
-    for row in funcionarios:
-        row_num += 1
-        for col_num in range(len(row)):
-            ws.write(row_num, col_num, row[col_num], font_style)          
-    
-    wb.save(response)
-    return response
-    '''
     except:    
         print('Exceção!')
         wb.save(response)
-        return response'''
+        return response
 
     
 def buscarFuncionarios():
 
     """ Retornar todos os funcionarios que tem supervisor """
-    query = Funcionario.objects.all().filter().values_list('codigo', 'nome', 'supervisor', 'cooperativa')
+    query = Funcionario.objects.all().filter().values_list('codigo', 'nome', 'supervisor', 'cooperativa', 'meta')
     funcionarios = []
     for i in query:
         if i[2] != None:
-            funcionarios.append([i[0], i[1], i[2], i[3]]) # codigo, nome, supervisor, empresa
+            funcionarios.append([i[0], i[1], i[2], i[3], i[4]]) # codigo, nome, supervisor, empresa, meta
     return funcionarios
 
 
@@ -253,15 +267,18 @@ def countFaltas(datas, funcionario):
     return faltas
 
 
-def buscarProducoes(funcionarios, datas):
+def buscarProducoes(funcionarios, datas, vrPago):
     faltas = 0
     diasUteis = countDiasUteis(datas)
     media = 0.00
     mediaEfetiva = 0.00
+    producaoTotal = 0.00
 
     for j in range(len(funcionarios)):
 
         funcionario = Funcionario.objects.get(codigo=funcionarios[j][0])
+        funcionarios[j].append(producaoTotal) # Adiciona produção total do período
+        funcionarios[j].append(vrPago) # Adiciona vr que será pago por kg
         funcionarios[j].append(diasUteis - countFaltas(datas, funcionario)) # Adiciona dias totais trabalhados
         funcionarios[j].append(diasUteis) # Adiciona dias úteis do período
         funcionarios[j].append(media) # Adiciona média de produção do período
@@ -280,14 +297,15 @@ def buscarProducoes(funcionarios, datas):
 
     # Calculando as médias
     for i in funcionarios:
-        coluna = 7
-        for j in range(len(i) - 7): # Subtrai as primeiras colunas
-            media += i[coluna]
+        coluna = 11
+        for j in range(len(i) - 11): # Subtrai as primeiras colunas
+            producaoTotal += i[coluna]
             coluna += 1
-        i[6] = media / i[5] # Média de produção
-        i[7] =  media / i[4] # Média efetiva de produção
+        i[5] = producaoTotal
+        i[9] = round(producaoTotal / i[8], 2) # Média de produção
+        i[10] =  round(producaoTotal / i[7], 2) # Média efetiva de produção
 
-        media = 0
+        producaoTotal = 0
 
     return funcionarios
 
