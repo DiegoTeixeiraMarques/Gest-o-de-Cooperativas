6e1e28c711a456ad8030e162715b571906734483
Diego Teixeira <diegoteixeira1996@gmail.com>
2020-10-21 12:36:10 -0300

    Alteração para usar portas com1, com2 etc de forma dinamica

--- CoopSys/producao/views.py
+++ CoopSys/producao/views.py
@@ -92,44 +92,51 @@ def relatoriodiadia(request):
 
 def pegarPeso():
 
-    try:
-        __message = str(chr(5))
-        _serial = serial.Serial(
-            'com1',
-            baudrate=9600,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=1,
-            writeTimeout=2
-        )
+    def pegarPeso():
+    listaPortas = ['com4', 'com3', 'com2', 'com1', 'com5']
+    porta = 0
+    new_weight = ""
+    while new_weight == "":
 
-        #print(_serial)
+        try:
+            __message = str(chr(5))
+            _serial = serial.Serial(
+                listaPortas[porta],
+                baudrate=9600,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=1,
+                writeTimeout=2
+            )
 
-        # if True
-        if _serial.isOpen():
-            #print("aberto")
-            _serial.write(__message.encode('ascii'))
-            _serial.flushInput()
-            weight = _serial.read(14)
-            str_weight = str(weight.decode())
-            # new_weight = ["0","3",".","1","2"]
-            new_weight = []
-            for x in range(len(str_weight)):
-                if 0 < x < 7:
-                    new_weight.append(str_weight[x])
+            #print(_serial)
 
-            _serial.close()
+            # if True
+            if _serial.isOpen():
+                #print("aberto")
+                _serial.write(__message.encode('ascii'))
+                _serial.flushInput()
+                weight = _serial.read(14)
+                str_weight = str(weight.decode())
+                # new_weight = ["0","3",".","1","2"]
+                new_weight = []
+                for x in range(len(str_weight)):
+                    if 0 < x < 7:
+                        new_weight.append(str_weight[x])
 
-            new_weight = "".join(new_weight)
-        else:
-            #print("fechado")
+                _serial.close()
+
+                new_weight = "".join(new_weight)
+                
+            else:
+                #print("fechado")
+                new_weight = 0
+        except:
+            #print("exceção na pesagem")
             new_weight = 0
-    except:
-        #print("exceção")
-        new_weight = 35
-    
-
+        porta = porta + 1
+        #print(listaPortas[porta])
     return (new_weight)
 
 
